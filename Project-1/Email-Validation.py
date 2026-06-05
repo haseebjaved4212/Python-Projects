8551b31d7eb06af01a2d42d7da51b2dacd1352e6
haseebjaved4212 <m.haseebjavedhj@gmail.com>
2026-06-05 19:30:11 +0500

    feat: implement basic email format validation script

---
+++ Project-1/Email-Validation.py
@@ -0,0 +1,19 @@
+email = input("Enter your email :")
+if len(email)>=6:
+    pass
+else:
+    print("Invalid Email")
+if "@" in email:
+    pass
+else:
+    print("Invalid Email")
+if "." in email:
+    pass
+else:
+    print("Invalid Email")
+if "@" and "." in email:
+    pass
+else:
+    print("Invalid Email")
+    
+print("Valid Email")
