c908b242940ee6e42d096f801f2ae9b780e7cf49
haseebjaved4212 <m.haseebjavedhj@gmail.com>
2026-06-07 17:57:33 +0500

    feat: add phone number details lookup script using phonenumbers library

--- 04-Phone-Number-Details/main.py
+++ 04-Phone-Number-Details/main.py
@@ -5,7 +5,7 @@ from phonenumbers import geocoder,carrier,timezone
 num=input("Enter your phone number with country code: ")
 ch=geocoder.description_for_number(phonenumbers.parse(num),"en")
 cr=carrier.name_for_number(phonenumbers.parse(num),"en")
-tz=timezone.time_for_number(phonenumbers.parse(num),"en")
+tz=timezone.time_zones_for_number(phonenumbers.parse(num))
 
 print("Country: ",ch)
 print("Operator: ",cr)
